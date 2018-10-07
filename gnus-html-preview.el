68682ab6862149db5ba19d10eeedba7111b95d47
Patrick Smyth <patricksmyth01@gmail.com>
2018-10-07 19:15:48 -0400

    Added needle haystack unility funciton, refactored with it

--- gnus-html-preview.el
+++ gnus-html-preview.el
@@ -1,15 +1,19 @@
+(defun gnus-preview-needle-haystack (needle haystack)
+  "Find location of string NEEDLE in string HAYSTACK."
+  (string-match-p (regexp-quote needle) haystack.))
+
+
 (defun gnus-preview-grab-html ()
   
   (let* ((buffer-string (buffer-substring (point-min) (point-max)))
-	 (html-mimepart-preamble-location (string-match-p (regexp-quote "<#part type=text/html>") buffer-string))
-	 (html-mimepart-postamble-location (string-match-p (regexp-quote "<#/multipart>") buffer-string)))
+	 (html-mimepart-preamble-location (gnus-preview-needle-haystack "<#part type=text/html>" buffer-string))
+	 (html-mimepart-postamble-location (gnus-preview-needle-haystack "<#/multipart>" buffer-string))
     (if html-mimepart-preamble-location
 	(with-temp-buffer
 	  "*ghost-buff*"
 	  (insert buffer-string)
 	  (buffer-substring (+ 23 html-mimepart-preamble-location) html-mimepart-postamble-location))
-      (message "No HTML mimepart to preview"))))
-
+      (message "No HTML mimepart to preview")))))
 
 
 (defun gnus-preview-write-html ()
@@ -26,3 +30,6 @@
   (progn
     (gnus-preview-write-html)
     (browse-url "file:/tmp/emailpreview.html")))
+
+
+

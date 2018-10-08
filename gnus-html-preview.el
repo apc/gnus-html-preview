ef373b249c54de4e1690e2459acf61d512cfc71d
Patrick Smyth <patricksmyth01@gmail.com>
2018-10-07 21:27:49 -0400

    Made the email preview look fancy, added headers to the preview

--- gnus-html-preview.el
+++ gnus-html-preview.el
@@ -1,34 +1,62 @@
 (defun gnus-preview-needle-haystack (needle haystack)
   "Find location of string NEEDLE in string HAYSTACK."
-  (string-match-p (regexp-quote needle) haystack.))
+  (string-match-p (regexp-quote needle) haystack))
+
+
+(defun gnus-preview-replace-line-breaks (string)
+  "Replaces all line breaks in STRING with HTML <br> tags."
+  (replace-regexp-in-string "\n" "<br>" string))
+
+
+(defun gnus-preview-escape-angle-brackets (string)
+  "Escape all angle brackets in STRING."
+  (replace-regexp-in-string ">" "&gt" (replace-regexp-in-string "<" "&lt" string)))
+
+
+(defun gnus-preview-grab-segment (string-begin string-end &optional shift-begin)
+  "Get string from buffer that begins with STRING-BEGIN and ends with STRING-END."
+  (let* ((buffer-string (buffer-substring (point-min) (point-max)))
+	 (html-mimepart-preamble-location (gnus-preview-needle-haystack string-begin buffer-string))
+	 (html-mimepart-postamble-location (gnus-preview-needle-haystack string-end buffer-string))
+	 (shift (if shift-begin shift-begin 0)))
+    (if html-mimepart-preamble-location
+	(with-temp-buffer
+	  "**ghost-buff**"
+	  (insert buffer-string)
+	  (buffer-substring (+ shift html-mimepart-preamble-location) html-mimepart-postamble-location))
+      (message "No HTML mimepart to preview"))))
 
 
 (defun gnus-preview-grab-html ()
-  
-  (let* ((buffer-string (buffer-substring (point-min) (point-max)))
-	 (html-mimepart-preamble-location (gnus-preview-needle-haystack "<#part type=text/html>" buffer-string))
-	 (html-mimepart-postamble-location (gnus-preview-needle-haystack "<#/multipart>" buffer-string))
-    (if html-mimepart-preamble-location
-	(with-temp-buffer
-	  "*ghost-buff*"
-	  (insert buffer-string)
-	  (buffer-substring (+ 23 html-mimepart-preamble-location) html-mimepart-postamble-location))
-      (message "No HTML mimepart to preview")))))
+  "Get the HTML mimepart from current message buffer."
+  (gnus-preview-grab-segment "<#part type=text/html>" "<#/multipart>" 23))
 
 
-(defun gnus-preview-write-html ()
+(defun gnus-preview-grab-heading ()
+  "Grab some info from the beginning of the email to display along with the preview."
+  (gnus-preview-grab-segment "To:" "--text follows this line--" 1))
+
+
+(defun gnus-preview-write ()
   (interactive)
-  (let ((html-to-write (gnus-preview-grab-html)))
-    (if html-to-write
-
+  (let ((html (gnus-preview-grab-html))
+	(heading (gnus-preview-grab-heading)))
+    (if html
 	(with-temp-file "/tmp/emailpreview.html"
-	  (insert html-to-write)))))
+	  (insert "<html><head><title>Email Preview</title><style>.indented {margin: 0 10% 0 10%}</style></head><body><pre class='indented'>")
+	  (insert (gnus-preview-replace-line-breaks (gnus-preview-escape-angle-brackets heading)))
+	  (insert "</pre>")
+	  (insert "<hr><main class='indented'>")
+	  (insert html)
+	  (insert "</main></body></html>")))))
+
+
 
 
 (defun gnus-preview-current-email ()
   (interactive)
   (progn
-    (gnus-preview-write-html)
+    (gnus-preview-write)
     (browse-url "file:/tmp/emailpreview.html")))
 
 
